bb54278c9a0b64d97fd030693b6bac8663c29221
kwabena369 <68285748+kwabena369@users.noreply.github.com>
2024-10-07 13:15:18 +0000

    changes_

---
+++ ecom/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:ecom/Screens/Glosory/ProductListScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+//   the stating pint of the application
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  //   the building of the real deal
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: "Ecoms", home: ProductListScreen());
+  }
+}
--- ecommenceapp/android/app/src/main/kotlin/com/example/ecommenceapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ecommenceapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
