847a9c497ad799c93e181cd61b90ef972551977b
TexasStudio <68285748+kwabena369@users.noreply.github.com>
2024-11-27 13:09:31 +0000

    Update Authen.dart

--- ecom/lib/Service/Authen.dart
+++ ecom/lib/Service/Authen.dart
@@ -4,7 +4,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 class AuthService {
   static final FirebaseAuth _auth = FirebaseAuth.instance;
   static final GoogleSignIn _googleSignIn = GoogleSignIn();
-
+//golden piece
   static Future<Map<String, dynamic>?> signInWithGoogle() async {
     try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
